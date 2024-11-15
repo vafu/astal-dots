c462b45c32a40599aa1fa31f8490f47dca78ebca
Valentyn <valentyn.fuchedzhy@gmail.com>
2024-11-15 00:08:05 -0800

    more jsx

--- app.ts
+++ app.ts
@@ -1,24 +1,13 @@
-import { App, Astal, Gdk } from "astal/gtk4"
-import { Widget } from "astal/gtk4"
+import { App } from "astal/gtk4"
 import style from "./style.scss"
 import Adw from "gi://Adw?version=1"
-import Workspaces from "./widgets/workspaces"
+import Bar from "./windows/Bar"
 
 App.start({
     css: style,
     main() {
         Adw.init()
-        App.get_monitors().forEach(m => Bar(m).catch(e => console.log(e)))
+        App.get_monitors().forEach(m => Bar(m))
     },
 })
 
-
-export default async function Bar(gdkmonitor: Gdk.Monitor) {
-    const w = new Widget.Window({
-        gdkmonitor: gdkmonitor,
-        name: "name",
-        exclusivity: Astal.Exclusivity.EXCLUSIVE,
-        anchor: Astal.WindowAnchor.TOP | Astal.WindowAnchor.LEFT | Astal.WindowAnchor.RIGHT,
-    }, Workspaces())
-    return w
-}
